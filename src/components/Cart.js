dc0d42e9a55af05c1a1a14ff9daae589272084ea
Tuhin Khan <webwithjhankarvai@gmail.com>
2021-03-02 11:34:42 +0600

    running

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,16 +3,20 @@ import React from 'react';
 const Cart = (props) => {
     // console.log(props);
     const cart = props.cart;
+
+    const total = cart.reduce((total,player) => total + player.salary , 0)
     return (
         <div>
-            <h1 style={{color:'red'}}>Player Details </h1>
+            <h1 style={{color:'green'}}>Player Details </h1>
             <h2>Player Added : {props.cart.length}</h2>
+             <h3 style={{color:'salmon'}}>Expense : {total} TK</h3>
             {
                 cart.map((c)=> {
                     return ( 
                         <div>
                             <h3>Name: {c.name}</h3>
-                            <p>Salary: {c.salary}</p>
+                            <p>Salary: {c.salary} TK</p>
+                            
                         </div>
                      )
                             
